84c8fe568974ef5e34b3dc5a6b1c3296a37d4e6d
Christian Scholtysik <99259326+ChristianScholtysik@users.noreply.github.com>
2024-09-04 13:08:01 +0200

    Update ExpenseField.tsx

--- src/components/ExpenseField.tsx
+++ src/components/ExpenseField.tsx
@@ -1,14 +1,17 @@
 import ButtonExpense from "./ButtonExpense";
 
-const ExpenseField = () => {
+interface ExpenseFieldProps {
+  text: string;
+}
+
+const ExpenseField: React.FC<ExpenseFieldProps> = ({ text }) => {
   return (
     <section>
       <div className="flex items-center bg-gray rounded-full py-2 px-4 gap-2">
         <ButtonExpense />
         <div className="flex flex-col justify-end">
           <p className=" text-small">Expenses</p>
-          <p className=" text-small font-semibold">+ € 4,302</p>
-          {/* TODO: Zahlenfeld Dynamisch machen */}
+          <p className=" text-xs font-semibold">-{text}</p>
         </div>
       </div>
     </section>
