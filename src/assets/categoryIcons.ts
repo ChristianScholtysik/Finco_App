acd42f8a05c0ba39bbe2ca6c58ba12fffc824f59
shakedown3000 <annalaube90@gmail.com>
2024-09-04 21:12:40 +0200

    edit_report

---
+++ src/assets/categoryIcons.ts
@@ -0,0 +1,15 @@
+const categoryIcons: Record<string, string> = {
+  "Food & Drink": "🍕",
+  Salary: "💵",
+  Insurance: "🧯",
+  "Clothing & Accessories": "👚",
+  "Entertainment & Leisure": "🎮",
+  "Travel & Vacation": "🏖️",
+  "Utilities (Electricity, Water, Gas)": "🪫",
+  "Rent/Mortgage": "🏠",
+  "Healthcare & Medical Expenses": "⛑️",
+  "Dining Out & Takeaway": "🥂",
+  Other: "💰",
+};
+
+export default categoryIcons;
