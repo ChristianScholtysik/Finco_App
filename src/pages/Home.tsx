06382227d992d0d1e2da5468233fee745ec69b82
JulianSupercode <jscheib1305@gmail.com>
2024-09-04 09:17:34 +0200

    Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,8 @@ import supabaseClient from "../lib/supabaseClient";
 import { useUserContext } from "../context/UserContext";
 import { useProfileData } from "../context/ProfileContext";
 import CreditCard from "../components/CreditCard";
+import ExpenseFieldXL from "../components/ExpenseFieldXL";
+import IncomeFieldXL from "../components/IncomeFieldXL";
 
 const Home = () => {
   const { profile, setProfile } = useProfileData();
@@ -42,14 +44,32 @@ const Home = () => {
   }
 
   return (
-    <>
-      <p>Card Number: {profile.card_number}</p>
-      <p>
-        Card Holder: {profile.first_name} {profile.last_name}
-      </p>
-      <CreditCard />
+    <div className="flex items-center justify-center">
+      <section className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm">
+        <div className="flex items-center justify-between mb-5">
+          <div>
+            <h2 className="font-Urbanist text-sm">Welcome back</h2>
+            <p className="font-Urbanist text-lg">
+              {profile.first_name} {profile.last_name}
+            </p>
+          </div>
+
+          <img
+            className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+            alt="Profile"
+          />
+        </div>
+
+        <CreditCard />
+        <p className="font-Urbanist text-lg mb-6 mt-7">Total wallet</p>
+        <div className="flex gap-5 mb-10">
+          <IncomeFieldXL />
+          <ExpenseFieldXL />
+        </div>
+      </section>
       <Navbar />
-    </>
+    </div>
   );
 };
 
